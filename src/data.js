5a68b331221462027c27633a1480d3de06e76a06
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2023-07-25 06:54:06 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "NGO Website",
+    subtitle: "HTML CSS and JavaScript",
+    description:
+      "The website is te snippet of the website made for an education related NGO made through purely HTML and CSS",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Mangata and Gallo",
+    subtitle: "HTML and CSS",
+    description:
+      "Made a project for course completion has all the features a website can have while providing a complete experience.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Calculator",
+    subtitle: "JavaScript",
+    description:
+      "A fast paces calculator for all your calculations and to keep track of all the money that comes in for you. Easy to you with all the basic operations.",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "A todo list for all your day to day activities and to keep you from forgetting the necessary jobs to do so that you can always be on the schedule.",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I had asked for a website for my end of year project. The website delivered was very easy to use and had a very unique design along with colour combinations that complimented each other in a beautiful way. The website was developed in a timely manner without much delay.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Ayushman Rao",
+    company: "senior",
+  },
+  {
+    quote:
+      "We required a website for a group project in the science fest of our college and as none of us possesed the required skills we asked Swapnil for one and we can attest to the fact that all our teammates along with the judges werre more than satisfied.",
+    image: "https://randomuser.me/api/portraits/women/94.jpg",
+    name: "Anamika Jain",
+    company: "ShopEase",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Bootstrap",
+  "ReactJS",
+  "MySQL",
+];
